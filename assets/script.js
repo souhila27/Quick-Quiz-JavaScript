68bf1a9de612bd9b052ba01674c9695f4eb84e76
souhila boukari <suhamimisou14@gmail.com>
2022-02-23 22:30:11 -0500

    adding if statment

--- assets/script.js
+++ assets/script.js
@@ -70,3 +70,104 @@ var myQuestions = [
         correctAnswer: "quotes",
     },
 ];
+function checkAnswer(elem) {
+    // This removes the focus on the button after clicking
+    if (elem.textContent != myQuestions[currentQuestion].correctAnswer) {
+        currentTime = currentTime - 5;
+    }
+
+    currentQuestion = currentQuestion + 1;
+
+    if (currentQuestion >= myQuestions.length) {
+        gameOver();
+    } else {
+        generateQuestion();
+    }
+}
+function checkTime(elem) {
+    if (currentQuestion == myQuestions.length) {
+        clearInterval(elem);
+
+    } else if (currentTime <= 0) {
+        time.textContent = 0;
+        currentTime = 0;
+        clearInterval(elem);
+        gameOver();
+    }
+}
+
+// function resetQuiz() {
+//     var user_initials = document.querySelector("#user_initials").value;
+
+//     localStorage.setItem("highScore", currentTime);
+//     localStorage.setItem(user_initials, currentTime);
+// }
+
+
+function gameOver() {
+    /*
+    // happy path
+    if they answer all the questions
+    get time left from timer
+    show it as the score
+    ask the user for initials
+    save score and initials to high scores
+
+    //unhappy path
+    timer hit zero, they get no points
+    alert a box that says you lose
+
+
+
+*/
+
+    // stop the timer - done
+    // get the time left 
+    var score = currentTime;
+    // -- unhide our intialsSection
+    // reach into the html to grab the form
+    // change the forms style display to block
+    var initFormEl = document.querySelector(".iniform");
+    initFormEl.style.display = "block";
+
+    // reach into the html get the submit button
+    
+    function submitBtnEl() {
+       var user_initials = document.querySelector("#user_initials").value;
+        localStorage.setItem("highScore", currentTime);
+        localStorage.setItem(user_initials, currentTime);
+        
+    }
+    // add and event listent to the submitbutton
+    // submitBtnEl.addEventListener('click', function () {
+
+    // }
+    // -- event.preventDefault()
+    // -- reach into the thml and get the input box value
+    var initialsElement = document.querySelector('#user_initials')
+    // -- add it to some variable
+
+    // at that point you should ave a var w/ the score 
+    // and a var that holds the initials
+
+
+
+
+    // var score_list = document.querySelector(".score-list");
+    // var clearClick = document.querySelector("#clearScores");
+
+    // for (var i = 0; i < localStorage.length; i++) {
+    //     // This checks to make sure that the high score stored in local storage is not displayed on the high score list.
+    //     if (localStorage.key(i) != "highScore") {
+    //         var item = document.createElement("li");
+    //         item.textContent = localStorage.key(i) + " - " + localStorage.getItem(localStorage.key(i));
+    //         score_list.appendChild(item);
+    //     }
+    // }
+
+    // // This clears the local storage and reloads the page.
+    // clearClick.addEventListener("click", function () {
+    //     localStorage.clear();
+    //     location.reload();
+    // })
+};
